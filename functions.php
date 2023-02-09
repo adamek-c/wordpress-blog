ace162252a3e36e5d0278e167345373de36b128d
Adam Ciołek <adasciolek95@gmail.com>
2023-02-09 11:10:51 +0100

    add connect files with header and footer, add css,js,fonts to header

--- functions.php
+++ functions.php
@@ -1,1 +1,18 @@
-Hello this is a custom theme
+<?php 
+
+// Add nessesery settings for example: css,js to our custom theme 
+if(!function_exists('add_settings')) {
+
+  function add_settings() {
+    wp_enqueue_style('custom-font-archivo', "//fonts.googleapis.com/css2?family=Archivo:wght@300;400;500;600;700&display=swap");
+    wp_enqueue_style('custom-font-work-sans', "//fonts.googleapis.com/css2?family=Work+Sans:wght@400;500;600&display=swap");
+    wp_enqueue_style('custom-style', get_theme_file_uri("./build/style-index.css"));
+    wp_enqueue_script('custom-js', get_theme_file_uri("./build/index.js"), array(), 1.0, true);
+  }
+
+}
+
+
+
+add_theme_support('title-tag');
+add_action('init', 'add_settings');
---
+++ header.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="<?php language_attributes();?>">
+  <head>
+    <meta charset="<?php bloginfo('charset');?>">
+    <?php wp_head(); ?>
+  </head>
+  <body>
+
+
+  
