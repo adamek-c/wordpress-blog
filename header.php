e7d37bc003d9b60dcd1e6ff5f7645414256371b1
Adam Ciołek <adasciolek95@gmail.com>
2023-02-13 14:58:28 +0100

    add style to gallery and fix header style

--- header.php
+++ header.php
@@ -1,23 +1,24 @@
 <!DOCTYPE html>
 <html lang="<?php language_attributes();?>">
-  <head>
-    <meta charset="<?php bloginfo('charset');?>">
-    <?php wp_head(); ?>
-  </head>
-  <body>
+<head>
+  <meta charset="<?php bloginfo('charset');?>">
+  <?php wp_head(); ?>
+</head>
+<body>
 
   <header>
     <nav>
-      <div class="container nav__menu">
-        <a class="nav__menu__logo" href="<?php echo site_url('') ?>">Blogo</a>
+      <div class="container">
+        <div class="nav__menu">
+          <a class="nav__menu__logo" href="<?php echo site_url('') ?>">Blogo</a>
 
-        <?php 
-          wp_nav_menu(array(
-            'theme_location' => 'main-menu',
-          ));
-        ?>
+          <?php 
+            wp_nav_menu(array(
+              'theme_location' => 'main-menu',
+            ));
+          ?>
+        </div>
       </div>
     </nav>
   </header>
   <main>
-  
