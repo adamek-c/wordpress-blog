39e5d857cd9be72acad0af9ed00d7b82177f3bee
Adam Ciołek <adasciolek95@gmail.com>
2023-02-14 15:14:22 +0100

    display blog posts and add paginate_links

--- index.php
+++ index.php
@@ -1,12 +1,52 @@
 <?php get_header(); ?>
 
   <div class="container">
-    <div>
-      <div></div>
-      <div>
-        <?php get_sidebar();?>
+   <div class="blog">
+      <div class="blog__content">
+        <?php 
+          $paged = ( get_query_var( 'paged' ) ) ? get_query_var( 'paged' ) : 1;
+          $blogposts = new WP_Query(array(
+            'post_type' => 'post',
+            'posts_per_page' => 7,
+            'post_status' => 'publish',
+            'orderby' => 'date',
+            'order' => 'DESC',
+            'paged' => $paged,
+          ));
+
+          if($blogposts->have_posts()) : ?>
+            <?php  
+              while($blogposts->have_posts()) : $blogposts->the_post() ?>
+              <div>
+                <?php the_post_thumbnail('medium'); ?>
+                <div>
+                  <h4><?php the_title(); ?></h4>
+                  <p><?php  echo wp_trim_words(get_the_content(), 18); ?></p>
+                </div>
+              </div>
+              <?php endwhile; ?>
+              <div class="blog__content__paginate">
+                <?php 
+                  $big = 999999999; 
+                
+                  echo paginate_links( array(
+                    'base' => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
+                    'format' => '?paged=%#%',
+                    'current' => max( 1, get_query_var('paged') ),
+                    'total' => $blogposts->max_num_pages,
+                    'prev_text' => '<<',
+                    'next_text' => '>>',
+
+                  ) );
+                ?>
+              </div>
+           <?php ?>
+            <?php wp_reset_postdata(); ?>
+            <?php endif;
+          ?>
       </div>
-    </div>
+      <?php get_sidebar();?>
+    </div> 
   </div>
 
 <?php get_footer(); ?>
