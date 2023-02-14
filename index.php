7a886bef29e34b44ec47cfd22c0ca385c02ca181
Adam Ciołek <adasciolek95@gmail.com>
2023-02-14 17:23:13 +0100

    fixed stick sidebar, add more styles to blog page, display blog post

--- index.php
+++ index.php
@@ -17,11 +17,12 @@
           if($blogposts->have_posts()) : ?>
             <?php  
               while($blogposts->have_posts()) : $blogposts->the_post() ?>
-              <div>
-                <?php the_post_thumbnail('medium'); ?>
+              <div class="blog__content__info">
+                <?php the_post_thumbnail('post-thumbnails_xl'); ?>
                 <div>
-                  <h4><?php the_title(); ?></h4>
-                  <p><?php  echo wp_trim_words(get_the_content(), 18); ?></p>
+                  <h4 class="blog__content__info__title"><?php the_title(); ?></h4>
+                  <p class="blog__content__info__text"><?php  echo wp_trim_words(get_the_content(), 18); ?></p>
+                  <a class="blog__content__info__link" href="<?php the_permalink(); ?>">Czytaj Dalej</a>
                 </div>
               </div>
               <?php endwhile; ?>
