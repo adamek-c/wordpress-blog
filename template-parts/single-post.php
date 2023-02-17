822fbaa01a081ddd1278efd847acce910179d707
Adam Ciołek <adasciolek95@gmail.com>
2023-02-17 10:09:48 +0100

    add time read field to single post

--- template-parts/single-post.php
+++ template-parts/single-post.php
@@ -2,6 +2,11 @@
     if(have_posts()) :
       while(have_posts()) : the_post() ?>
         <h1 class="single-post__content__title"><?php the_title(); ?></h1>
+          <?php 
+            if(get_field('time_read')) { ?>
+              <span class="single-post__content__time">Time read <strong><?php echo get_field('time_read'); ?></strong> min</span>
+            <?php }
+          ?>
         <?php the_post_thumbnail('large'); ?>
         <div class="single-post__content__text">
           <?php the_content(); ?>
